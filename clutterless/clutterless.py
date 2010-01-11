ab7d20682f3befb2113e97c1ca7212f3b5b3d710
Clovis Fabricio <mercurial.m.nosklo@0sg.net>
2010-01-11 20:05:05 -0200

    New special set that stores everyone you talk to. That gives them a x5 boost on timeouts (so you see them leaving even if it takes some time)

--- clutterless/clutterless.py
+++ clutterless/clutterless.py
@@ -105,15 +105,22 @@ class ActiveChannel(object):
         self._lineno = 0
         self._lineno_data = {}
         self._linecut = linecut
-        
-        self._checks = (self._lastseen, self._timeout_data, self._lineno_data)
 
-    def register(self, key):
+        self._special = set()
+        
+        self._checks = (self._lastseen, self._timeout_data, self._lineno_data, 
+                        self._special)
+
+    def register(self, nick):
         """Registers a new key"""
-        self._timeout_data[key] = self._time()
-        self._lineno_data[key] = self._lineno
-        self._lastseen.add(key)
+        self._timeout_data[nick] = self._time()
+        self._lineno_data[nick] = self._lineno
+        self._lastseen.add(nick)
         self._lineno += 1
+
+    def register_special(self, nick):
+        self._special.add(nick)
+        self.register(nick)
 
     def show(self):
         timeout_data = ' '.join('%s[%.2f]' % (nick, time.time() - t)
@@ -126,21 +133,26 @@ class ActiveChannel(object):
                                          key=operator.itemgetter(1)))
 
         lastseen_data = ','.join(self._lastseen)
+        special_data = ','.join(self._special)
         
-        return 'Timeout(%s), Lineno(%s), LastSeen(%s)' % (timeout_data,
-                                                          lineno_data,
-                                                          lastseen_data)
+        return 'Timeout(%s), Lineno(%s), LastSeen(%s), Special(%s)' % (
+                timeout_data, lineno_data, lastseen_data, special_data)
 
     def clean(self):
         self._cleandict(self._timeout_data, self._time(), self.timeout)
         self._cleandict(self._lineno_data, self._lineno, self._linecut)
         self._lastseen.clean()
+        self._special = set(nick for nick in self._special 
+                            if any(nick in check for check in self._checks[:-1]))
                 
     def _cleandict(self, d, base, cut):
         for key, value in d.items():
             value = base - value
-            if value > cut:
-                logger.debug('cleaning %s for %r<%r ', key, value, cut)
+            use_cut = cut
+            if key in self._special:
+                use_cut *= 5
+            if value > use_cut:
+                logger.debug('cleaning %s for %r>%r ', key, value, cut)
                 del d[key]
         
     def autocleans(func):
@@ -165,12 +177,14 @@ class ActiveChannel(object):
         return any(user in check for check in self._checks)
 
     def rename(self, nick1, nick2):
-        logger.debug("Renaming %r to %r", nick1, nick2)
         self._lastseen.rename(nick1, nick2)
         for d in (self._timeout_data, self._lineno_data):
             if nick1 in d:
                 d[nick2] = d[nick1]
                 del d[nick1]
+        if nick1 in self._special:
+            self._special.remove(nick1)
+            self._special.add(nick2)            
         
 class JoinPartFilter(object):
     def __init__(self):
@@ -179,7 +193,6 @@ class JoinPartFilter(object):
                     'Channel Action',
                     'Channel Action Hilight',
                     'DCC CHAT Offer',
-                    'Message Send',
                     'Channel Message',
                     'Channel Msg Hilight',
                 ):
@@ -192,15 +205,42 @@ class JoinPartFilter(object):
                     'Quit',
                 ):
             xchat.hook_print(supressed, self.supress, userdata=supressed)
+        
+        
         xchat.hook_print('Change Nick', self.rename, priority=xchat.PRI_LOW)
+        
+        xchat.hook_print('Your Message', self.extract_nick)
+        for special in (
+                    'Message Send',
+                ):
+            xchat.hook_print(special, self.special, userdata=special)
         
         xchat.hook_command('clutterdebug', self.cmd_debug)
         xchat.hook_command('cluttershow', self.cmd_show)
+#        xchat.hook_command('clutter', self.cmd_clutter)
+
 
     def _get_channel(self):
         info = xchat.get_context().get_info
         return tuple(info(t) for t in ('host', 'channel'))
-        
+    
+    def extract_nick(self, word, word_eol, *data):
+        """People you talk to becomes *special* - can have more timeout"""
+        nick, sep, rest = word[1].partition(xchat.get_prefs('completion_suffix'))
+        nick = remove_mirc_color(nick)
+        if sep and ' ' not in nick:
+            # a nick, register as special
+            logger.debug('Registering special nick %r', nick)
+            activechan = self.active[self._get_channel()]
+            activechan.register_special(nick)
+
+    def special(self, word, word_eol, userdata):
+        nick = remove_mirc_color(word[0])
+        logger.info('special called %r', word)
+#        activechan = self.active[self._get_channel()]
+#        logger.debug("%r(%d) special register for %r: %r", userdata, activechan._lineno, 
+#                                                           nick, word)
+#        activechan.register(nick)
 
     def action(self, word, word_eol, userdata):
         nick = remove_mirc_color(word[0])
@@ -220,6 +260,7 @@ class JoinPartFilter(object):
     def rename(self, word, word_eol, userdata):
         nick1 = remove_mirc_color(word[0])
         nick2 = remove_mirc_color(word[1])
+        logger.debug("Renaming %r to %r", nick1, nick2)
         # rename happens serverwide
         thishost = xchat.get_context().get_info('host')
         for (host, channel), active in self.active.iteritems():
